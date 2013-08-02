78324ee899ffae3a0df231dcf5a74c7f58908d0f
StephanG <sg@organicwire.de>
2013-08-02 07:09:22 +0200

    Date & time recognition works.

--- clean_bam_files.php
+++ clean_bam_files.php
@@ -14,23 +14,37 @@ $protected_filenames = array(
   'test.txt',
 );
 
-$pattern = '/(02|03|04|05|06|07|08|09|10|11|13|14|15|16|17|18|19|20|21|22|23).\d+.\d+.mysql(.gz)?/';
+$pattern_datetime = '/(\d{4}).(\d{2}).(\d{2})[T_](\d{2}).(\d{2}).(\d{2}).mysql(.gz)?$/';
 
 if ($handle = opendir($path)) {
+    $files_info = array();
+  
     /* This is the correct way to loop over the directory. */
     while (false !== ($entry = readdir($handle))) {
-        if (in_array($entry, $protected_filenames) ) {
+      // Exclude special files
+      if (in_array($entry, $protected_filenames) ) {
         continue;
       }
       
-      if (preg_match($pattern, $entry)) {
-        echo "Delete $entry\n";
-        
-      }
-      else {
-        echo "Skipping $entry\n";
+      // Determine date and time of backup files
+      $matches = array();
+      preg_match($pattern_datetime, $entry, $matches);
+      if (isset($matches[0])) {
+        // print_r($matches);
+        $files_info[$matches[0]] = array(
+          'year' => $matches[1],
+          'month' => $matches[2],
+          'day' => $matches[3],
+          'hour' => $matches[4],
+          'min' => $matches[5],
+          'second' => $matches[6],
+        );
       }
     }
+    
+    // Run deletion. Keep a backup per day
+    
+    print_r($files_info);
 }
 
 ?>
