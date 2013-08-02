bd4ea632ea0bd129a6ba929a4a5794c767a5f457
StephanG <sg@organicwire.de>
2013-08-02 10:06:54 +0200

    Create README.md

--- clean_bam_files.php
+++ clean_bam_files.php
@@ -4,18 +4,7 @@
 /**
  * Keep backup&migrate's scheduled folder small and lean.
  * 
- * Delete backup files in the following way
- * - today:             keep all backups
- * - this & last week:  keep one backup per day
- * - all the rest:      keep one backup per week
- * 
- * Usage:
- *  clean_bam_files.php <directory>
- *  
- * Example:
- *  clean_bam_files.php /var/www/myproject/sites/default/files/backup_migrate/scheduled
- *  
- * This script works with both drupal 6 and 7 (actually, it doesn't require any drupal instance).
+ * See README.md
  */
 
 // TODO: Option -f to do an actual delete.
