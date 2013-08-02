af05110d3d195e91f63f12336fa3ad68860ebe04
StephanG <sg@organicwire.de>
2013-08-02 07:34:16 +0200

    Seems to properly work.

--- clean_bam_files.php
+++ clean_bam_files.php
@@ -31,21 +31,56 @@ if ($handle = opendir($path)) {
       preg_match($pattern_datetime, $entry, $matches);
       if (isset($matches[0])) {
         // print_r($matches);
-        $files_info[$matches[0]] = array(
+        $files_info[$path . '/' . $matches[0]] = array(
           'year' => $matches[1],
           'month' => $matches[2],
           'day' => $matches[3],
           'hour' => $matches[4],
           'min' => $matches[5],
           'second' => $matches[6],
+          'delete' => FALSE,
         );
       }
     }
-    
-    // Run deletion. Keep a backup per day
-    
-    print_r($files_info);
 }
+
+ksort($files_info);
+    
+// Prepare deletion. Keep a backup per day
+foreach ($files_info as $file => &$info) {
+  // Check if backup for this day exists
+  $files_for_day = file_for_date($files_info, $info['year'], $info['month'], $info['day']);
+  if (count($files_for_day) > 1) {
+    $info['delete'] = TRUE;
+  }
+}
+
+// Do actual delete
+foreach ($files_info as $file => &$info) {
+  if ($info['delete']) {
+    echo "Delete $file";
+  }
+  else {
+    echo "Keep   $file";
+  }
+  echo "\n";
+}
+
+/**
+ * Returns file names for a given date.
+ * Consider only those files which were not marked for deletion.
+ */
+function file_for_date($files_info, $year, $month, $day) {
+  $ret = array();
+  foreach($files_info as $file => $info) {
+    if (!$info['delete'] && $info['year'] == $year && $info['month'] == $month && $info['day'] == $day) {
+      $ret[] = $file;
+    }
+  }
+  return $ret;
+}
+    
+// print_r($files_info);
 
 ?>
 
